33293ed2776f2b1ee31756aa512e4e588c81134a
Vladimir Solo <freeman.wushu@mail.ru>
2026-02-23 11:27:47 +0300

    refactor: create product table component

---
+++ src/views/ProductsList/_components/ProductsTable/index.tsx
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import { Avatar, Button, Flex, Table, Typography } from "antd";
+import type { TableProps } from "antd";
+import { EllipsisOutlined, UserOutlined } from "@ant-design/icons";
+import css from "./index.module.scss";
+import Column from "antd/es/table/Column";
+import { getProducts } from "@/api";
+import { useQuery } from "@tanstack/react-query";
+import { useDebounce } from "@/hooks/useDebounce";
+import { removeExtraSpaces } from "@/utils/removeExtraSpaces";
+
+import type { Product } from "@/types";
+import { useTableParams } from "../../_hooks/useTableParams";
+
+type TableRowSelection<T extends object = object> =
+  TableProps<T>["rowSelection"];
+
+type Props = {
+  search: string;
+};
+
+export const ProductsTable = ({ search }: Props) => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  const debouncedSearch = useDebounce(removeExtraSpaces(search));
+  const { sortBy, order, sorter, currentPage, pageSize, updateParams } =
+    useTableParams();
+
+  const { data, isLoading } = useQuery({
+    queryKey: [
+      "products",
+      debouncedSearch,
+      sorter?.field,
+      sorter.order,
+      currentPage,
+      pageSize,
+    ],
+    queryFn: () =>
+      getProducts({
+        search: debouncedSearch,
+        sortBy,
+        order,
+        limit: pageSize,
+        skip: (currentPage - 1) * pageSize,
+      }),
+  });
+
+  const rowSelection: TableRowSelection<Product> = {
+    selectedRowKeys,
+    onChange: setSelectedRowKeys,
+  };
+
+  return (
+    <Table<Product>
+      scroll={{ y: "calc(100vh - 400px)" }}
+      rowSelection={rowSelection}
+      dataSource={data?.products ?? []}
+      loading={isLoading}
+      rowKey="id"
+      pagination={{
+        current: currentPage,
+        pageSize,
+        total: data?.total,
+        showSizeChanger: true,
+      }}
+      onChange={(pagination, _, sorterArg) => {
+        const single = Array.isArray(sorterArg) ? sorterArg[0] : sorterArg;
+        console.log(sorterArg);
+        updateParams({
+          currentPage: pagination.current,
+          pageSize: pagination.pageSize,
+          field: single?.field as string | undefined,
+          order:
+            single?.order === "ascend"
+              ? "asc"
+              : single?.order === "descend"
+                ? "desc"
+                : undefined,
+        });
+      }}
+    >
+      <Column<Product>
+        key="title"
+        ellipsis
+        render={(_: unknown, row) => (
+          <Flex align="center" gap={12}>
+            <Avatar shape="square" size={48} icon={<UserOutlined />} />
+            <Flex vertical align="start">
+              <span>{row.title}</span>
+              <span>{row.category}</span>
+            </Flex>
+          </Flex>
+        )}
+        title="Наименование"
+        width={300}
+      />
+      <Column<Product>
+        key="brand"
+        ellipsis
+        render={(_: unknown, row) => <span>{row.brand}</span>}
+        title="Вендор"
+      />
+      <Column<Product>
+        key="sku"
+        ellipsis
+        render={(_: unknown, row) => <span>{row.sku}</span>}
+        title="Артикул"
+      />
+      <Column<Product>
+        key="rating"
+        ellipsis
+        dataIndex="rating"
+        sorter
+        sortOrder={sorter.field === "rating" ? sorter.order : undefined}
+        render={(_: unknown, row) => (
+          <>
+            {row.rating && (
+              <>
+                <Typography.Text
+                  type={row?.rating && row.rating < 3 ? "danger" : undefined}
+                >
+                  {row.rating}
+                </Typography.Text>
+                <span>/5</span>
+              </>
+            )}
+          </>
+        )}
+        title="Оценка"
+      />
+      <Column<Product>
+        dataIndex="price"
+        key="price"
+        ellipsis
+        sorter
+        sortOrder={sorter.field === "price" ? sorter.order : undefined}
+        render={(_: unknown, row) => <span>{row.price} ₽</span>}
+        title="Цена, ₽"
+      />
+      <Column<Product>
+        key="add"
+        ellipsis
+        render={() => <button className={css.addBtn}>+</button>}
+        width={100}
+      />
+      <Column<Product>
+        key="info"
+        ellipsis
+        render={() => (
+          <Button className={css.infoBtn} icon={<EllipsisOutlined />} />
+        )}
+        width={100}
+      />
+    </Table>
+  );
+};
--- src/views/ProductsList/index.tsx
+++ src/views/ProductsList/index.tsx
@@ -1,58 +1,14 @@
-import React, { useState } from "react";
-import { Avatar, Button, Flex, Input, Table, Typography } from "antd";
-import type { TableProps } from "antd";
-import {
-  EllipsisOutlined,
-  PlusCircleOutlined,
-  SearchOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { useState } from "react";
+import { Button, Flex, Input, Typography } from "antd";
+import { PlusCircleOutlined, SearchOutlined } from "@ant-design/icons";
 import RefreshIcon from "@/assets/ArrowsClockwise.svg?react";
 import css from "./index.module.scss";
-import Column from "antd/es/table/Column";
-import { getProducts } from "@/api";
-import { useQuery } from "@tanstack/react-query";
-import { useDebounce } from "@/hooks/useDebounce";
-import { removeExtraSpaces } from "@/utils/removeExtraSpaces";
-import { useTableParams } from "./_hooks/useTableParams";
-import type { Product } from "@/types";
 import { AddProductModal } from "./_components/AddProductModal";
-
-type TableRowSelection<T extends object = object> =
-  TableProps<T>["rowSelection"];
+import { ProductsTable } from "./_components/ProductsTable";
 
 export const ProductsList = () => {
-  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
   const [search, setSearch] = useState("");
   const [isOpenModal, setIsOpenModal] = useState(false);
-
-  const debouncedSearch = useDebounce(removeExtraSpaces(search));
-  const { sortBy, order, sorter, currentPage, pageSize, updateParams } =
-    useTableParams();
-
-  const { data, isLoading } = useQuery({
-    queryKey: [
-      "products",
-      debouncedSearch,
-      sorter?.field,
-      sorter.order,
-      currentPage,
-      pageSize,
-    ],
-    queryFn: () =>
-      getProducts({
-        search: debouncedSearch,
-        sortBy,
-        order,
-        limit: pageSize,
-        skip: (currentPage - 1) * pageSize,
-      }),
-  });
-
-  const rowSelection: TableRowSelection<Product> = {
-    selectedRowKeys,
-    onChange: setSelectedRowKeys,
-  };
 
   return (
     <div className={css.container}>
@@ -92,109 +48,7 @@ export const ProductsList = () => {
           </Flex>
         </Flex>
 
-        <Table<Product>
-          scroll={{ y: "calc(100vh - 400px)" }}
-          rowSelection={rowSelection}
-          dataSource={data?.products ?? []}
-          loading={isLoading}
-          rowKey="id"
-          pagination={{
-            current: currentPage,
-            pageSize,
-            total: data?.total,
-            showSizeChanger: true,
-          }}
-          onChange={(pagination, _, sorterArg) => {
-            const single = Array.isArray(sorterArg) ? sorterArg[0] : sorterArg;
-
-            updateParams({
-              currentPage: pagination.current,
-              pageSize: pagination.pageSize,
-              field: single?.field as string | undefined,
-              order:
-                single?.order === "ascend"
-                  ? "asc"
-                  : single?.order === "descend"
-                    ? "desc"
-                    : undefined,
-            });
-          }}
-        >
-          <Column<Product>
-            key="title"
-            ellipsis
-            render={(_: unknown, row) => (
-              <Flex align="center" gap={12}>
-                <Avatar shape="square" size={48} icon={<UserOutlined />} />
-                <Flex vertical align="start">
-                  <span>{row.title}</span>
-                  <span>{row.category}</span>
-                </Flex>
-              </Flex>
-            )}
-            title="Наименование"
-            width={300}
-          />
-          <Column<Product>
-            key="brand"
-            ellipsis
-            render={(_: unknown, row) => <span>{row.brand}</span>}
-            title="Вендор"
-          />
-          <Column<Product>
-            key="sku"
-            ellipsis
-            render={(_: unknown, row) => <span>{row.sku}</span>}
-            title="Артикул"
-          />
-          <Column<Product>
-            key="rating"
-            ellipsis
-            dataIndex="rating"
-            sorter
-            sortOrder={sorter.order}
-            render={(_: unknown, row) => (
-              <>
-                {row.rating && (
-                  <>
-                    <Typography.Text
-                      type={
-                        row?.rating && row.rating < 3 ? "danger" : undefined
-                      }
-                    >
-                      {row.rating}
-                    </Typography.Text>
-                    <span>/5</span>
-                  </>
-                )}
-              </>
-            )}
-            title="Оценка"
-          />
-          <Column<Product>
-            dataIndex="price"
-            key="price"
-            ellipsis
-            sorter
-            sortOrder={sorter.order}
-            render={(_: unknown, row) => <span>{row.price} ₽</span>}
-            title="Цена, ₽"
-          />
-          <Column<Product>
-            key="add"
-            ellipsis
-            render={() => <button className={css.addBtn}>+</button>}
-            width={100}
-          />
-          <Column<Product>
-            key="info"
-            ellipsis
-            render={() => (
-              <Button className={css.infoBtn} icon={<EllipsisOutlined />} />
-            )}
-            width={100}
-          />
-        </Table>
+        <ProductsTable search={search} />
       </Flex>
 
       {isOpenModal && (
